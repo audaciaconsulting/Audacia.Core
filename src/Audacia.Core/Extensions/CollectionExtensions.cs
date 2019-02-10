724249966e0c144b89b48ccb96751c9243d86336
John Penny <john.penny@audacia.co.uk>
2019-02-10 11:58:42 +0000

    - .AddRange() on an ICollection - Check if an underlying Type .IsNumeric()

--- src/Audacia.Core/Extensions/CollectionExtensions.cs
+++ src/Audacia.Core/Extensions/CollectionExtensions.cs
@@ -11,5 +11,23 @@ namespace Audacia.Core.Extensions
                 collection.Add(item);
             }
         }
+        
+        /// <summary>
+        /// Adds the elements of the specified collection to the end of the List.
+        /// </summary>
+        public static void AddRange<T>(this ICollection<T> source, IEnumerable<T> range)
+        {
+            if (source is List<T> list)
+            {
+                list.AddRange(range);
+            }
+            else
+            {
+                foreach (var item in range)
+                {
+                    source.Add(item);
+                }
+            }
+        }
     }
 }
