cbefbcb8ff8ffec6e0da78e8205b2d60ae96f330
Hasham Babar <hasham.babar@audacia.co.uk>
2024-10-24 10:46:15 +0100

    Updated target framework to .NET8 and adjustments for .NET8

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## 2.0.0 - 2024-10-24
+- Updated target framework to .NET8
+- Minor adjustments to support .NET8
+- Changed `PagingRequest` and `SortablePagingRequest` to records
+
 ## 1.2.0 - 2024-07-08
 ### Added
 - Added useful extension methods used at Audacia ([244adfa](https://github.com/audaciaconsulting/Audacia.Core/pull/3/commits/244adfab31d9035c332daaf4b53a4bff71278e1c))
--- src/Audacia.Core/Paging/PagingRequest.cs
+++ src/Audacia.Core/Paging/PagingRequest.cs
@@ -3,36 +3,7 @@
     /// <summary>
     /// Request to apply paging to a list of results.
     /// </summary>
-    public class PagingRequest
-    {
-        /// <summary>
-        /// Request to apply paging to a list of results with default values.
-        /// </summary>
-        public PagingRequest()
-            : this(int.MaxValue, 0)
-        {
-            /* Required for injection */
-        }
-
-        /// <summary>
-        /// Request to apply paging to a list of results.
-        /// </summary>
-        /// <param name="pageSize">The number of results to show per page.</param>
-        /// <param name="pageNumber">Which page of results to return.</param>
-        public PagingRequest(int pageSize = int.MaxValue, int pageNumber = 0)
-        {
-            PageSize = pageSize;
-            PageNumber = pageNumber;
-        }
-
-        /// <summary>
-        /// Gets or sets the number of results we'll show per page.
-        /// </summary>
-        public int? PageSize { get; set; }
-
-        /// <summary>
-        /// Gets or sets which page of results to return. Pages start at 1.
-        /// </summary>
-        public int PageNumber { get; set; }
-    }
+    /// <param name="PageSize"> Gets or sets the number of results we'll show per page. </param>
+    /// <param name="PageNumber"> Gets or sets which page of results to return. Pages start at 1. </param>
+    public record PagingRequest(int PageSize = int.MaxValue, int PageNumber = 0);
 }
