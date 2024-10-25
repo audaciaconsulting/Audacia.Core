cb664a801d070004145d5d5aea731b20aebc31d2
Hasham Babar <hasham.babar@audacia.co.uk>
2024-10-25 07:30:20 +0100

    reverted removal of getter and setter

--- src/Audacia.Core/Paging/SortablePagingRequest.cs
+++ src/Audacia.Core/Paging/SortablePagingRequest.cs
@@ -1,12 +1,38 @@
 ﻿namespace Audacia.Core
 {
-    /// <summary>
+/// <summary>
     /// Request to sort, and apply paging to a list of results.
     /// </summary>
-    /// <param name="PageSize">The size of the page.</param>
-    /// <param name="PageNumber">The page number.</param>
-    /// <param name="SortProperty">The property to sort by.</param>
-    /// <param name="Descending">Indicates if the sorting should be in descending order.</param>
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Maintainability", "AV1564:Parameter in public or internal member is of type bool or bool?", Justification = "Makes sense when passed from UI.")]
-    public record SortablePagingRequest(int PageSize = int.MaxValue, int PageNumber = 0, string SortProperty = "", bool Descending = false) : PagingRequest(PageSize, PageNumber);
+    public record SortablePagingRequest : PagingRequest
+    {
+        /// <summary>
+        /// Initializes a new instance of <see cref="SortablePagingRequest"/>.
+        /// </summary>
+        public SortablePagingRequest()
+            : this(int.MaxValue, 0)
+        {
+            /* Required for injection */
+        }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="SortablePagingRequest"/>.
+        /// </summary>
+        /// <param name="pageSize">The size of the page.</param>
+        /// <param name="pageNumber">The page number.</param>
+        public SortablePagingRequest(int pageSize = int.MaxValue, int pageNumber = 0)
+            : base(pageSize, pageNumber)
+        {
+            SortProperty = string.Empty;
+        }
+
+        /// <summary>
+        /// Gets or sets the property of the destination type we'll sort by.
+        /// </summary>
+        public string SortProperty { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether gets or sets a value indicating the sort direction.
+        /// </summary>
+        public bool Descending { get; set; }
+    }
 }
